cb8a54d4e36abf5fcb0d633822573238c9c54f76
Anand Dinakar <ananddinakar@gmail.com>
2019-01-06 00:21:58 -0500

    added visdom visualization;

--- train.py
+++ train.py
@@ -1,25 +1,7 @@
-def train(data_loader, net, criterion, optimizer, epoch):
-    net.train(True)
-    train_loss = 0.
-    for i, (inputs, labels) in enumerate(data_loader, 0):
-        print("Training epoch %d: batch # %d" % (epoch, i), end='\r')
-        # map to gpu
-        inputs, labels = inputs.cuda(), labels.cuda()
+import numpy as np
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        
-        train_loss += loss.item()
-    return train_loss / len(data_loader)
 
 def validate(data_loader, net, criterion, measures, epoch):
-    net.train(False)
     val_loss = 0.
     measurements = {k:0. for k in measures.keys()}
     for i, (inputs, labels) in enumerate(data_loader, 0):
@@ -40,12 +22,39 @@ def validate(data_loader, net, criterion, measures, epoch):
         measurements[k] = measurements[k] / len(data_loader)
     return val_loss / len(data_loader), measurements
 
-def fit(net, train_loader, val_loader, criterion, optimizer, lrscheduler, measures, epoch):
-    train_loss = train(train_loader, net, criterion, optimizer, epoch)
-    val_loss, measurements = validate(val_loader, net, criterion, measures, epoch)
-    lrscheduler.step(val_loss)
+def fit(net, train_loader, val_loader, criterion, optimizer, lrscheduler, measures, epoch, vis):
     
-    print("epoch {}: train-loss={:.5f}, val-loss={:.5f}, new_lr={:.5f}".format(epoch, train_loss, val_loss, optimizer.param_groups[0]['lr']))
-    measurements['train_loss'] = train_loss
+    net.train(True)
+    train_loss = 0.
+    epoch_size = len(train_loader)
+    losses=[]
+    for i, (inputs, labels) in enumerate(train_loader, 0):
+        print("Training epoch %d: batch # %d" % (epoch, i), end='\r')
+        # map to gpu
+        inputs, labels = inputs.cuda(), labels.cuda()
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        train_loss += loss.item()
+        
+        losses.append(loss.item())
+        if i % 30 == 0:
+            vis.plot_loss(np.mean(losses), (epoch_size * epoch) + i, 'train_loss')
+            losses.clear()
+            
+            net.train(False)
+            val_loss, measurements = validate(val_loader, net, criterion, measures, epoch)
+            vis.plot_loss(val_loss, (epoch_size * epoch) + i, 'val_loss')
+            net.train(True)
+            lrscheduler.step(val_loss)
+        
+    measurements['train_loss'] = train_loss / epoch_size
     measurements['val_loss'] = val_loss
     return measurements
