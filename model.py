2207d348f9cfcab984a7cfd99e46751ae6930cf9
Anand Dinakar <ananddinakar@gmail.com>
2019-01-10 10:10:12 -0500

    simplified model

--- model.py
+++ model.py
@@ -13,10 +13,10 @@ class Net(nn.Module):
         self.encoder = vgg16.features
         for i,param in enumerate(self.encoder.parameters()):
             param.requires_grad = i >= 16
-        self.decoder1x1 = nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.convT2d1 = nn.ConvTranspose2d(in_channels=1, out_channels=512, kernel_size=4, stride=2, padding=1)
-        self.convT2d2 = nn.ConvTranspose2d(in_channels=1024, out_channels=256, kernel_size=4, stride=4, padding=0)
-        self.convT2d3 = nn.ConvTranspose2d(in_channels=512, out_channels=1, kernel_size=4, stride=4, padding=0)
+
+        self.a_convT2d = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1)
+        self.b_convT2d = nn.ConvTranspose2d(in_channels=768, out_channels=128, kernel_size=4, stride=4, padding=0)
+        self.convT2d3 = nn.ConvTranspose2d(in_channels=384, out_channels=1, kernel_size=4, stride=4, padding=0)
     
     def forward(self, x):
 
@@ -25,15 +25,15 @@ class Net(nn.Module):
             x = layer(x)
             if i in [23, 15]:
                 skipConnections[i] = x
-                
-        x = self.decoder1x1(x)
-        x = self.convT2d1(x)
+
+        x = self.a_convT2d(x)
         x = torch.cat((x,skipConnections[23]), 1)
-        x = self.convT2d2(x)
+        x = self.b_convT2d(x)
         x = torch.cat((x, skipConnections[15]), 1)
+
         x = self.convT2d3(x)
         x = nn.Sigmoid()(x)
         x = x.view(x.size()[0], -1, Net.num_classes)
-        
+
         return x
 
